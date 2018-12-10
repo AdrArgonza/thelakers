4cc5d4596082c6f1c2c650677568a8788dc74532
AC <acargonza2@gmail.com>
2018-12-09 22:46:17 -0800

    php file

--- phpTest.php
+++ phpTest.php
@@ -1,11 +1,11 @@
 <?php
     
     //Connect to the database
-    $host = "localhost";
+    $host = "0.0.0.0";
     $user = "aargonzafonza";                     //Your Cloud 9 username
     $pass = "";                                  //Remember, there is NO password by default!
     $db = "sample_db";                                  //Your database name you want to connect to
-    $port = 3306;                                //The port #. It is always 3306
+    $port = 8080;                                //The port #. It is always 3306
     
     $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
 
