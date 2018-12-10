8b7624d6446b46c5fb885d3813a5f2473cb2edf8
AC <acargonza2@gmail.com>
2018-12-09 20:57:55 -0800

    php file

--- phpTest.php
+++ phpTest.php
@@ -1,7 +1,7 @@
 <?php
     
     //Connect to the database
-    $host = "127.0.0.1";
+    $host = "localhost";
     $user = "aargonzafonza";                     //Your Cloud 9 username
     $pass = "";                                  //Remember, there is NO password by default!
     $db = "sample_db";                                  //Your database name you want to connect to
