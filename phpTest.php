d761254fdaac3e6bb07f9e7d5f496212c4105ca8
AdrArgonza <44507358+AdrArgonza@users.noreply.github.com>
2018-12-10 11:42:47 -0800

    php file

--- phpTest.php
+++ phpTest.php
@@ -1,11 +1,11 @@
 <?php
     
     //Connect to the database
-    $host = "$localhost";
+    $host = "c9.io";
     $user = "aargonzafonza";                     //Your Cloud 9 username
     $pass = "";                                  //Remember, there is NO password by default!
-    $db = "sample_db";                                  //Your database name you want to connect to
-    $port = 3306;                                //The port #. It is always 3306
+    $db = "the_lakers";                          //Your database name you want to connect to
+    $port = 310;                                //The port #. It is always 3306
     
     $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
 
@@ -16,4 +16,3 @@
     while ($row = mysqli_fetch_assoc($result)) {
         echo "The ID is: " . $row['id'] . " and the Username is: " . $row['username'];
     }
-
