6784fa7f710c7be15ebf7d0aedc022fef8b1e4dc
AC <acargonza2@gmail.com>
2019-01-04 04:03:23 -0800

    better file

--- thelakers.php
+++ thelakers.php
@@ -2,16 +2,9 @@
 		include_once 'TheStartingLineup.html';
 		require_once 'login.php';
 		$conn = mysqli_connect($host, $user, $pass, $db, $port);
-		if($conn->connect_error) die($conn->connect_error);
-		$option = isset($_POST['the_lakers']) ? $_POST['the_lakers'] : false;
-		if ($option){
-			$testField = $_POST['the_lakers'];
-			$query = "	select ". $testField . " from players;";
-			$result = mysqli_query($conn, $query);
+			$query = "SELECT * FROM players";
+			$result = mysqli_query($connection, $query);
 			while ($row = mysqli_fetch_assoc($result)) {
-				echo $row[$testField] . "<br>";
-			}
+			echo . $row['playername'] . . $row['url'] . . $row['playernumber'] . . $row['playerposition'] . . $row['PPG'] . . $row['height'] . . $row['weight'] . . $row['age'] . . $row['yeardrafted'] . . $row['yearsintheleague'] . . $row['playertext'];
 		}
-		$result->close();
-		$conn->close();
 ?>
