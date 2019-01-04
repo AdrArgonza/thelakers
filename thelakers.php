fb9d2b2c84e0b6f93d81ceb370d19f7d8a5031ff
Adrell Argonza <argonza.adrell1@gmail.com>
2019-01-04 19:10:48 +0000

    better files

--- thelakers.php
+++ thelakers.php
@@ -3,7 +3,7 @@
 		require_once 'login.php';
 		$conn = mysqli_connect($host, $user, $pass, $db, $port);
 			$query = "SELECT * FROM players";
-			$result = mysqli_query($connection, $query);
+			$result = mysqli_query($conn, $query);
 			while ($row = mysqli_fetch_assoc($result)) {
 			echo " Player's Name: " . $row['playername'] . "URL: ". $row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
 		}
