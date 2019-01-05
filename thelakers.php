40d99edd9957d46c2d21f5f6fd45744b1cdd975c
AC <acargonza2@gmail.com>
2019-01-04 21:05:39 -0800

    a change

--- thelakers.php
+++ thelakers.php
@@ -2,7 +2,7 @@
 		include_once 'TheStartingLineup.html';
 		require_once 'login.php';
 		$conn = mysqli_connect($host, $user, $pass, $db, $port);
-			$query = "SELECT * FROM players";
+			$fetch="SELECT playername,url,playernumber,playerposition,PPG,height,weight,age,yeardrafted,yearsintheleague,playertext FROM players WHERE playername = '".$playername."'";
 			$result = mysqli_query($conn, $query);
 			while ($row = mysqli_fetch_assoc($result)) {
 			echo " Player's Name: " . $row['playername'] . "URL: ". $row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
