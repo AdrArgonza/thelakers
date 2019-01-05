f23e2c5c3a44a9c3fd3b70bd0b77d0fe871321bd
AC <acargonza2@gmail.com>
2019-01-04 22:51:38 -0800

    Hope it works

--- thelakers.php
+++ thelakers.php
@@ -2,8 +2,9 @@
 		include_once 'TheStartingLineup.html';
 		require_once 'login.php';
 		$conn = mysqli_connect($host, $user, $pass, $db, $port);
-			$fetch="SELECT playername,url,playernumber,playerposition,PPG,height,weight,age,yeardrafted,yearsintheleague,playertext FROM players WHERE playername = '".$playername."'";
-			$result = mysqli_query($conn, $query);
+		if($conn->connect_error) die($conn->connect_error);
+		if(isset($_POST['submit'])){
+			$row = $_POST['player'];
 			while ($row = mysqli_fetch_assoc($result)) {
 			echo " Player's Name: " . $row['playername'] . "URL: ". $row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
 		}
