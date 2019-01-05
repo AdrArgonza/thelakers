30e3071064eabafa05818aa9eb1ef542047f3b21
AC <acargonza2@gmail.com>
2019-01-04 23:49:19 -0800

    hope it works

--- thelakers.php
+++ thelakers.php
@@ -3,7 +3,6 @@
 		require_once 'login.php';
 		$conn = mysqli_connect($host, $user, $pass, $db, $port);
 		if($conn->connect_error) die($conn->connect_error);
-		$query = "SELECT * FROM players";
 			while ($row = mysqli_fetch_assoc($result)) {
 			echo " Player's Name: " . $row['playername'] . "URL: ". $$row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
 		}
