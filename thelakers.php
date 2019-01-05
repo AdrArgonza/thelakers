f9792e829f881b0b9abea596c0a6436fab487d1f
AC <acargonza2@gmail.com>
2019-01-04 23:07:33 -0800

    hope it works

--- thelakers.php
+++ thelakers.php
@@ -3,8 +3,8 @@
 		require_once 'login.php';
 		$conn = mysqli_connect($host, $user, $pass, $db, $port);
 		if($conn->connect_error) die($conn->connect_error);
-		if(isset($_POST['submit'])){
-			$selected_val = $_POST['player'];
-			echo " Player's Name: " . $selected_val['playername'] . "URL: ". $selected_val['url'] . "Player's Number: " . $selected_val['playernumber'] . "Player's Position: " . $selected_val['playerposition'] . "Points Per Game:" . $selected_val['PPG'] . "Player's Height: " . $selected_val['height'] . "Player's Weight: " . $selected_val['weight'] . "Player's Age: " . $selected_val['age'] . "Year Drafted: " . $selected_val['yeardrafted'] . "Years in the League: " . $selected_val['yearsintheleague'] . "About the Player: " . $selected_val['playertext'];
+		while(isset($_POST['submit'])){
+			$row = $_POST['player'];
+			echo " Player's Name: " . $row['playername'] . "URL: ". $$row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
 		}
 ?>
