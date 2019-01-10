b35628f45021bbb8c98a5a137e9f32c9b298f179
Adrell Argonza <argonza.adrell1@gmail.com>
2019-01-10 18:44:26 +0000

    something

--- thelakers.php
+++ thelakers.php
@@ -7,3 +7,4 @@
 			echo " Player's Name: " . $row['playername'] . "URL: ". $$row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
 		}
 ?>
+SELECT * FROM players; 
