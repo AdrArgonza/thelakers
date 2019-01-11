8c6ebee1b86bed7b2274ae90f466eb9853be10ca
AC <acargonza2@gmail.com>
2019-01-10 23:31:26 -0800

    better file

--- thelakers.php
+++ thelakers.php
@@ -14,8 +14,3 @@
 			echo " Player's Name: " . $row['playername'] . "URL: ". $$row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
 }
 ?>
-<<<<<<< HEAD
-SELECT * FROM players; 
-=======
-
->>>>>>> 9db963047ec92872d6c6c3c431987b0bc441e929
