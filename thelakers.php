89d9671c68fdbfb52344169640894e7ef7c6649a
AC <acargonza2@gmail.com>
2019-01-10 23:41:39 -0800

    finished

--- thelakers.php
+++ thelakers.php
@@ -12,5 +12,6 @@
 		$result = mysqli_query($conn, $query);
 			while ($row = mysqli_fetch_assoc($result)) {
 			echo " Player's Name: " . $row['playername'] . "URL: ". $$row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
-}
+				}
+  }
 ?>
