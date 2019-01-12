bf8928443d8df9ac41bf52118fafadd35f0a3507
Adrell Argonza <argonza.adrell1@gmail.com>
2019-01-12 07:08:43 +0000

    better file

--- thelakers.php
+++ thelakers.php
@@ -4,14 +4,14 @@
 		$conn = mysqli_connect($host, $user, $pass, $db, $port);
 		if($conn->connect_error) die($conn->connect_error);
 		echo $_Post["playername"];
-		$option = isset($_POST['playername']) ? $_POST['playername']:false;
+		$option = isset($_POST['playername']) ? $_POST['playername'] : false;
 		echo $option;
 		if ($option){
 		$testField = $_POST['playername'];
 		$query = "select * from players where playername = '" . $testField."';";
 		$result = mysqli_query($conn, $query);
 			while ($row = mysqli_fetch_assoc($result)) {
-			echo " Player's Name: " . $row['playername'] . "URL: ". $$row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
+			echo " Player's Name: " . $row['playername'] . "URL: ". $row['url'] . "Player's Number: " . $row['playernumber'] . "Player's Position: " . $row['playerposition'] . "Points Per Game:" . $row['PPG'] . "Player's Height: " . $row['height'] . "Player's Weight: " . $row['weight'] . "Player's Age: " . $row['age'] . "Year Drafted: " . $row['yeardrafted'] . "Years in the League: " . $row['yearsintheleague'] . "About the Player: " . $row['playertext'];
 				}
   }
 ?>
